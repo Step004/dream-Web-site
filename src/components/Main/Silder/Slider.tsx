f3e840957e5b73c1dd7f37e61cc4fea2aac017f8
Kateryna1712 <146448232+Step004@users.noreply.github.com>
2024-12-23 16:55:12 +0200

    2

--- src/components/Main/Silder/Slider.tsx
+++ src/components/Main/Silder/Slider.tsx
@@ -2,9 +2,12 @@ import { useState, useEffect } from "react";
 import styles from "./slider.module.css";
 
 import confirmIcon from "../../../assets/confirm_icon.svg";
+import { useMediaQuery } from "react-responsive";
 
 const Slider = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const [seeMore, setSeeMore] = useState(false);
 
   const videos = [
     {
@@ -36,70 +39,86 @@ const Slider = () => {
   }, [activeIndex]);
 
   const handleClick = (index: number) => {
-    if (window.innerWidth <= 768) {
+    if (isMobile) {
       setActiveIndex(index === activeIndex ? null : index);
     }
+  };
+
+  const toggleSeeMore = () => {
+    setSeeMore((prev) => !prev);
   };
 
   return (
     <div className={styles.slider_container}>
       <h1 className={styles.title}>FILM IS A STORY</h1>
       <div className={styles.images_container}>
-        {videos.map((video, index) => (
-          <div
-            className={`${styles.image_container} ${
-              activeIndex === index ? styles.active : ""
-            }`}
-            key={index}
-            onMouseEnter={() =>
-              window.innerWidth > 768 && setActiveIndex(index)
-            }
-            onMouseLeave={() => window.innerWidth > 768 && setActiveIndex(null)}
-            onClick={() => handleClick(index)}
-          >
-            {activeIndex === index ? (
-              <iframe
-                src={video.url}
-                frameBorder="0"
-                allow="autoplay; encrypted-media"
-                allowFullScreen
-                title="YouTube video"
-                className={styles.video}
-              ></iframe>
-            ) : (
-              <img
-                loading="lazy"
-                className={styles.image}
-                src={video.thumbnail}
-                alt=""
-              />
-            )}
-            <div className={styles.image_description__container}>
-              <section>
-                <h2 className={styles.image_header}>
-                  {index === 0
-                    ? "3D Animations"
-                    : index === 1
-                    ? "Creative/Cinematic"
-                    : "Corporate"}
-                </h2>
-                <p className={styles.image_description}>Description</p>
-              </section>
-              <a className={styles.view_project_link} href="#">
-                View Project
-                <span>
-                  <img
-                    loading="lazy"
-                    className={styles.confirmIcon}
-                    src={confirmIcon}
-                    alt=""
-                  />
-                </span>
-              </a>
+        {videos.map((video, index) => {
+          // Умовне відображення для смартфонів
+          if (isMobile && !seeMore && index > 0) {
+            return null;
+          }
+
+          return (
+            <div
+              className={`${styles.image_container} ${
+                activeIndex === index ? styles.active : ""
+              }`}
+              key={index}
+              onMouseEnter={() => !isMobile && setActiveIndex(index)}
+              onMouseLeave={() => !isMobile && setActiveIndex(null)}
+              onClick={() => handleClick(index)}
+            >
+              {activeIndex === index ? (
+                <iframe
+                  src={video.url}
+                  frameBorder="0"
+                  allow="autoplay; encrypted-media"
+                  allowFullScreen
+                  title="YouTube video"
+                  className={styles.video}
+                ></iframe>
+              ) : (
+                <img
+                  loading="lazy"
+                  className={styles.image}
+                  src={video.thumbnail}
+                  alt=""
+                />
+              )}
+              <div className={styles.image_description__container}>
+                <section>
+                  <h2 className={styles.image_header}>
+                    {index === 0
+                      ? "3D Animations"
+                      : index === 1
+                      ? "Creative/Cinematic"
+                      : "Corporate"}
+                  </h2>
+                  <p className={styles.image_description}>Description</p>
+                </section>
+                <a className={styles.view_project_link} href="#">
+                  View Project
+                  <span>
+                    <img
+                      loading="lazy"
+                      className={styles.confirmIcon}
+                      src={confirmIcon}
+                      alt=""
+                    />
+                  </span>
+                </a>
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
+      {isMobile && (
+        <div className={styles.butCont}>
+          <button onClick={toggleSeeMore} className={styles.see_more_button}>
+            {seeMore ? "See Less" : "See More"}
+          </button>
+        </div>
+      )}
     </div>
   );
 };
